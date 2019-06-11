a64e00d8b0cdd00a604fea2b33edfc27017f0916
ytsunomoto <ytsunomoto@tech.softbank.co.jp>
2019-06-11 10:37:26 +0900

    update

---
+++ DotnetCoreDynamodbSample/Program.cs
@@ -0,0 +1,86 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using DotnetCoreDynamodbSample.Config;
+using DotnetCoreDynamodbSample.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreDynamodbSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // 設定ファイルの読み込み
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            // サービスの設定
+            var serviceCollection = new ServiceCollection();
+
+            var dynamoDbConfig = new DynamoDbConfig();
+            configuration.Bind("DynamoDb", dynamoDbConfig);
+
+            if (dynamoDbConfig.LocalMode)
+            {
+                serviceCollection.AddSingleton<IAmazonDynamoDB>(sp =>
+                {
+                    var clientConfig = new AmazonDynamoDBConfig { ServiceURL = dynamoDbConfig.LocalServiceUrl };
+                    return new AmazonDynamoDBClient(clientConfig);
+                });
+            }
+            else
+            {
+                serviceCollection.AddAWSService<IAmazonDynamoDB>();
+            }
+
+            serviceCollection.AddTransient<IDynamoDbService, DynamoDbService>();
+            var service = serviceCollection.BuildServiceProvider().GetService<IDynamoDbService>();
+
+            // サンプル実装
+            var tables = await service.ListTablesAsync();
+            Console.WriteLine(string.Join(",", tables));
+
+            var tableName = "DynamoSample";
+            if (!tables.Any(_ => _ == tableName))
+            {
+                await service.CreateTableAsync(new CreateTableRequest
+                {
+                    TableName = "DynamoSample",
+                    AttributeDefinitions = new List<AttributeDefinition>
+                {
+                    new AttributeDefinition
+                    {
+                        AttributeName = "Id",
+                        AttributeType = "N"
+                    }
+                },
+                    KeySchema = new List<KeySchemaElement>
+                {
+                    new KeySchemaElement
+                    {
+                        AttributeName = "Id",
+                        KeyType = "HASH"  //Partition key
+                    }
+                },
+                    ProvisionedThroughput = new ProvisionedThroughput
+                    {
+                        ReadCapacityUnits = 2,
+                        WriteCapacityUnits = 2
+                    }
+                });
+            }
+
+            // TODO insert, delete item の実装
+
+            await service.DeleteTableAsync(new DeleteTableRequest { TableName = tableName });
+        }
+    }
+}
---
+++ DotnetCoreDynamodbSample/Services/DynamoDbService.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCoreDynamodbSample.Services
+{
+    public interface IDynamoDbService
+    {
+        Task<List<string>> ListTablesAsync(ListTablesRequest request = default);
+        Task<Dictionary<string, AttributeValue>> GetItemsAsync(GetItemRequest request);
+        Task CreateTableAsync(CreateTableRequest request);
+        Task DeleteTableAsync(DeleteTableRequest request);
+    }
+
+    public class DynamoDbService : IDynamoDbService
+    {
+        private readonly IAmazonDynamoDB _amazonDynamoDb;
+
+        public DynamoDbService(IAmazonDynamoDB amazonDynamoDb)
+        {
+            _amazonDynamoDb = amazonDynamoDb;
+        }
+
+        public async Task<List<string>> ListTablesAsync(ListTablesRequest request = null)
+        {
+            var res = request == null ? await _amazonDynamoDb.ListTablesAsync()
+                : await _amazonDynamoDb.ListTablesAsync(request);
+            return res.TableNames;
+        }
+
+        public async Task<Dictionary<string, AttributeValue>> GetItemsAsync(GetItemRequest request)
+        {
+            var res = await _amazonDynamoDb.GetItemAsync(request);
+            return res.Item;
+        }
+
+        public async Task CreateTableAsync(CreateTableRequest request)
+        {
+            await _amazonDynamoDb.CreateTableAsync(request);
+        }
+
+        public async Task DeleteTableAsync(DeleteTableRequest request)
+        {
+            await _amazonDynamoDb.DeleteTableAsync(request);
+        }
+    }
+}
